4df7c44f64f4e50d50ecf42ebb45ab314a77bdf7
deanzaka <hdeanzak@id.ibm.com>
2020-07-19 18:02:34 +0700

    change get to use axios

--- src/services/bigFiveService.js
+++ src/services/bigFiveService.js
@@ -11,23 +11,8 @@ export const getTemplateService = async () => {
   };
 
   const res = await axios(options);
-  console.log(res);
-  const template = await handleResponse(res);
-  return template;
-};
-
-const handleResponse = async (response) => {
-  return response.text().then((text) => {
-    const data = text && JSON.parse(text);
-    if (!response.ok) {
-      if (response.status !== 200) {
-        window.location.reload(true);
-      }
-
-      const error = (data && data.message) || response.statusText;
-      return Promise.reject(error);
-    }
-
-    return data;
-  });
+  if (res.status !== 200) {
+    return Promise.reject(res.statusText);
+  }
+  return res.data;
 };
