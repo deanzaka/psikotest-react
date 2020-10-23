e2a82305d8452417c4deff6b30d8e749b9b9441d
deanzaka <hdeanzak@id.ibm.com>
2020-10-23 08:41:30 +0700

    setup for heroku deployment

---
+++ build/precache-manifest.41d0a7d27de78d17e3638fc57dd16b71.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129309c218cecc3f004193a3dfb09302",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6611009148129a8ed57",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "31e21d782d5707538e9e",
+    "url": "/static/js/2.5af51883.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.5af51883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6611009148129a8ed57",
+    "url": "/static/js/main.59557404.chunk.js"
+  },
+  {
+    "revision": "d986b6cbd398fe49b902",
+    "url": "/static/js/runtime-main.05b644f0.js"
+  },
+  {
+    "revision": "0487be3d94389381f7b3c446fe5fbfc8",
+    "url": "/static/media/HeaderLogo.0487be3d.svg"
+  },
+  {
+    "revision": "677fd107396496046dadc350e39033bd",
+    "url": "/static/media/Logo.677fd107.svg"
+  },
+  {
+    "revision": "80a4ef32f47bffdde6e347bd4ab399a1",
+    "url": "/static/media/Rectangle.80a4ef32.svg"
+  }
+]);
